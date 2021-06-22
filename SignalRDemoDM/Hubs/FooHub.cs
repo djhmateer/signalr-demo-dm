e23aa1fdab81dd3ef8de04ef54da237ae166487e
Dave Mateer <davemateer@gmail.com>
2021-06-22 09:56:51 +0100

    Add project files.

---
+++ SignalRDemoDM/Hubs/FooHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRDemoDM.Hubs
+{
+    public class FooHub : Hub
+    {
+        public async Task SendMessageToCaller(string user, string message)
+        {
+            await Clients.Caller.SendAsync("ReceiveMessageFoo", user, message);
+        }
+
+        public Task ThrowException()
+        {
+            // This will propagate to the client by default (with no settings needed)
+            throw new HubException("This error will be sent to the client!");
+        }
+
+        public Task ThrowNormalException()
+        {
+            throw new ApplicationException("An application exception");
+        }
+    }
+}
---
+++ SignalRDemoDM/wwwroot/js/foo.js
@@ -0,0 +1,49 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/fooHub").build();
+
+// Javascript ReceiveMessage function that is called by the Hub
+connection.on("ReceiveMessageFoo", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    li.textContent = `${user} says ${message}`;
+});
+
+connection.start().then(function () {
+    //document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+
+    //call the Hub method SendMessageToCaller
+    connection.invoke("SendMessageToCaller", user, message)
+        .catch(function (err) {
+            return console.error(err.toString());
+        });
+    event.preventDefault();
+});
+
+
+document.getElementById("throwExceptionButton").addEventListener("click", function (event) {
+    connection.invoke("ThrowException")
+        .catch(function (err) {
+            return console.error(err.toString());
+        });
+    event.preventDefault();
+});
+
+
+document.getElementById("throwNormalExceptionButton").addEventListener("click", function (event) {
+    connection.invoke("ThrowNormalException")
+        .catch(function (err) {
+            return console.error(err.toString());
+        });
+    event.preventDefault();
+});
